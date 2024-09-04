1a5a7e4b38e09165df1b4b7ba5abe05c14d27592
pallavi3105 <pallavidakhade31@gmail.com>
2024-09-04 11:31:22 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
